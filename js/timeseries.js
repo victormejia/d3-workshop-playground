e9a8fbcd5b0ce4ad1d910a8d380daf68c4e016af
Victor Mejia <victor.a.mejia@gmail.com>
2015-10-22 16:57:08 -0700

    adding timeseries

---
+++ js/timeseries.js
@@ -0,0 +1,67 @@
+(function() {
+  'use strict';
+  var el = d3.select('.timeseries'),
+      elWidth = parseInt(el.style('width'), 10),
+      elHeight = parseInt(el.style('height'), 10),
+      margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = elWidth - margin.left - margin.right,
+      height = elHeight - margin.top - margin.bottom;
+
+  var x = d3.time.scale()
+      .range([0, width]);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .tickFormat(d3.time.format('%m/%d'))
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .innerTickSize(-width)
+      .orient("left");
+
+  var line = d3.svg.line()
+      .interpolate('monotone')
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.value); });
+
+  var svg = el.append("svg")
+      .attr("width", elWidth)
+      .attr("height", elHeight)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.json("/data/timeseries.json", function(error, data) {
+    if (error) throw error;
+
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain(d3.extent(data, function(d) { return d.value; }));
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis);
+
+    var path = svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
+
+    // animate path
+    // src: http://bl.ocks.org/duopixel/4063326
+    var totalLength = path.node().getTotalLength();
+    path.attr("stroke-dasharray", totalLength + " " + totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .transition()
+        .duration(400)
+        .ease("linear")
+        .attr("stroke-dashoffset", 0);
+  });
+}());
